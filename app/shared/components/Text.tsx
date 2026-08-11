0b9e54d1394d61c061fc86f4b21058b71b05115e
zeXsi <saienko.danil123@mail.ru>
2026-08-11 13:31:26 +0300

    fix blog

--- app/shared/components/Text.tsx
+++ app/shared/components/Text.tsx
@@ -6,18 +6,28 @@ interface Props extends HTMLAttributes<HTMLElement> {
 }
 
 const HTML_BREAK_PATTERN = /<br\s*\/?>/gi;
+const HTML_TAG_PATTERN = /<\/?[a-z][^>]*>/i;
 const DOUBLE_LINE_BREAK_PATTERN = /(?:\r?\n\s*){2,}/g;
 const SINGLE_LINE_BREAK_PATTERN = /\r?\n/g;
+
+const halveLineBreaks = (value: string) => {
+  const count = value.match(SINGLE_LINE_BREAK_PATTERN)?.length ?? 0;
+  return '<br/>'.repeat(Math.ceil(count / 2));
+};
 
 export default function Text({ children, isReplace = false, ...rest }: Props) {
   if (!children) return null;
 
-  const htmlWithBreaks = isReplace
-    ? children
-        .replace(HTML_BREAK_PATTERN, '<p></p>')
-        .replace(DOUBLE_LINE_BREAK_PATTERN, '<p></p>')
-        .replace(SINGLE_LINE_BREAK_PATTERN, '')
-    : children.replace(SINGLE_LINE_BREAK_PATTERN, '<br/>');
+  const hasHtml = HTML_TAG_PATTERN.test(children);
+  const htmlWithBreaks =
+    isReplace && hasHtml
+      ? children
+          .replace(HTML_BREAK_PATTERN, '<br/><br/>')
+          .replace(DOUBLE_LINE_BREAK_PATTERN, halveLineBreaks)
+          .replace(SINGLE_LINE_BREAK_PATTERN, '')
+      : children
+          .replace(DOUBLE_LINE_BREAK_PATTERN, halveLineBreaks)
+          .replace(SINGLE_LINE_BREAK_PATTERN, '<br/>');
 
   if (isReplace) {
     return <div { ...rest } dangerouslySetInnerHTML={ { __html: htmlWithBreaks } } />;
