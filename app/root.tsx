e04b4aa1b2333b99b1cacd967e7ff76a81121cfa
zeXsi <saienko.danil123@mail.ru>
2026-08-24 14:13:42 +0000

    add meta yandex

--- app/root.tsx
+++ app/root.tsx
@@ -100,6 +100,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="yandex-verification" content="df7b83ca7d58271f" />
         {/* <script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js"></script> */}
         <Meta />
         <Links />
